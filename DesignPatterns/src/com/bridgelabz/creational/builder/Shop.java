5904de958fa673e3223e211751161bc7a66a23b6
VenomRishi <mhatrerishikesh@gmail.com>
2019-10-04 17:39:39 +0530

    changes made using web application

--- DesignPatterns/src/com/bridgelabz/creational/builder/Shop.java
+++ DesignPatterns/src/com/bridgelabz/creational/builder/Shop.java
@@ -22,7 +22,7 @@ public class Shop {
 		 * create another class PhoneBuilder who will responsible for that
 		 */
 //		Phone phone = new Phone("android", 8, 64, "snapdragon", 6.8, 5000);
-		Phone phone = new PhoneBuilder().setOs("Android").setRam(8).getPhone();
+		Phone phone = new PhoneBuilder().setOs("Android").setRam(8).setStorage(120).getPhone();
 		System.out.println(phone);
 
 	}
--- DesignPatterns/src/com/bridgelabz/creational/factoryphone/FactoryMain.java
+++ DesignPatterns/src/com/bridgelabz/creational/factoryphone/FactoryMain.java
@@ -13,7 +13,7 @@ public class FactoryMain {
 
 	public static void main(String[] args) {
 		OperatingSystemFactory operatingSystemFactory=new OperatingSystemFactory();
-		OS obj=operatingSystemFactory.getInstance("asda");
+		OS obj=operatingSystemFactory.getInstance("Open");
 		obj.spec();
 	}
 
